f168f48cf60460b8290e2ebaf541ee118278adcb
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-07-25 19:52:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Contacts from './components/Contacts'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <NavBar />
+      <Projects />
+      <Skills />
+      <Contacts />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const [ListNav] = useState(['home', 'skills', 'projects', 'contacts']);
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
