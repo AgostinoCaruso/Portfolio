31dd43ba0bed09819bd15a3b4ef3262f0171bff7
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-07-29 19:11:37 +0200

    fix media

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,55 +1,69 @@
-import React, { useState, useContext } from 'react'
-import { GlobalContext } from '../context/GlobalContext'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState, useContext, useEffect } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCss3Alt,
   faHtml5,
   faJs,
   faReact,
   faJava
-} from '@fortawesome/free-brands-svg-icons'
-
-import { faDatabase } from '@fortawesome/free-solid-svg-icons'
-
-import springLogo from '../assets/springLogo.png'
+} from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import springLogo from '../assets/springLogo.png';
 
 const Skills = () => {
-
   const { language, translation } = useContext(GlobalContext);
   const skills = translation[language].skills;
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 576);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 576);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section id="skills">
-
       <div data-aos="zoom-in">
-        <div className="title">
-          {translation[language].skills.title}
-        </div>
-        <div className="des">
-          {translation[language].skills.des}
-        </div>
+        <div className="title">{skills.title}</div>
+        <div className="des">{skills.des}</div>
       </div>
+
       <div className="list" data-aos="zoom-in">
-        {
-          skills.skillsList.map((value, index) => {
-            let aosType = '';
-            if (index % 3 === 0) aosType = 'fade-up';
-            else if (index % 3 === 1) aosType = 'zoom-in';
-            else aosType = 'fade-down';
+        {skills.skillsList.map((value, index) => {
+          let aosType = '';
 
-            return (
-              <div key={index} className='item' data-aos={aosType}>
-                {value.name === 'SPRING' ? <img src={value.icon} alt="" />
-                  : <FontAwesomeIcon icon={value.icon} />}
-                <h3>{value.name}</h3>
-                <div className="des">{value.des}</div>
-              </div>
-            );
-          })
-        }
+          if (isMobile) {
+            aosType = index % 3 === 0 ? 'fade-up'
+                    : index % 3 === 1 ? 'fade-down'
+                    : 'zoom-in';
+          } else {
+            aosType = index % 3 === 0 ? 'fade-right'
+                    : index % 3 === 1 ? 'zoom-in'
+                    : 'fade-left';
+          }
+
+          return (
+            <div key={index} className="item" data-aos={aosType}>
+              {value.name === 'SPRING'
+                ? <img src={value.icon} alt="" />
+                : <FontAwesomeIcon icon={value.icon} />}
+              <h3>{value.name}</h3>
+              <div className="des">{value.des}</div>
+            </div>
+          );
+        })}
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
